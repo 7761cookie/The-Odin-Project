77d776f6e1309894ddb7cf0b778fcdf14adb5cdd
7761cookie <s.w.cookland@gmail.com>
2024-05-11 00:31:48 +0100

    JavaScript Basics - Rock-Paper-Scissors

--- Foundations/rock-paper-scissors/script.js
+++ Foundations/rock-paper-scissors/script.js
@@ -1,48 +1,68 @@
 //console.log("Hello World")
 
-const choice = ["rock", "paper", "scissors"];  
-
-const randomNumber = (max) => {
-        return Math.floor(Math.random() * max);
-    };
-
-const n = randomNumber(3); 
-
-const humanInput = prompt("Pick: rock, paper or scissors");
-
-let humanScore = 0;
-let computerScore = 0;
 let rounds = 5;
 let roundsPlayed = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-const getComputerChoice = () => {
-    return choice[n];
-};
-
-const getHumanChoice = () => {
-    return humanInput.toLowerCase();
-}
-
-const playRound = (humanChoice, computerChoice) => {
-    if (getComputerChoice() == getHumanChoice()) {
-        return console.log("It's a draw!");
-    } else if (getComputerChoice() == "rock" && getHumanChoice() == "scissors" || getComputerChoice() == "paper" && getHumanChoice() == "rock") {
-        computerScore ++; 
-        return console.log("You Lose! " + getComputerChoice() + " beats " + getHumanChoice());
+const finalMessage = () => {
+    if (roundsPlayed == rounds && humanScore > computerScore) {
+        return console.clear(), console.log("You are the winner!")
+    } else if (roundsPlayed == rounds && humanScore < computerScore) {
+        return console.clear(), console.log("You are the Loser!")
+    } else if (roundsPlayed == rounds && humanScore == computerScore) {
+        return console.clear(), console.log("Its a draw")
     } else {
-        humanScore ++;
-        return console.log("You Won!");
+        return
     }
 }
-
 const playGame = () => {
-    do {
-        playRound(); 
-        roundsPlayed ++;
-    }
-    while (roundsPlayed != rounds) 
-}
-playGame();
-console.log("Computer Score: " + computerScore);
-console.log("Human Score: " + humanScore);
 
+    do {
+
+        const choice = ["rock", "paper", "scissors"];
+
+        const randomNumber = (max) => {
+            return Math.floor(Math.random() * max);
+        };
+
+        const n = randomNumber(3);
+
+        const getComputerChoice = () => {
+            return choice[n];
+        };
+
+        const humanInput = prompt("Pick: rock, paper or scissors");
+
+        const getHumanChoice = () => {
+            return humanInput.toLowerCase();
+        }
+
+        console.clear();
+
+        const playRound = (humanChoice, computerChoice) => {
+            if (getComputerChoice() == getHumanChoice()) {
+                return console.log("It's a draw!")
+            } else if (getComputerChoice() == "rock" && getHumanChoice() == "scissors" || getComputerChoice() == "paper" && getHumanChoice() == "rock") {
+                computerScore++;
+                return console.log("You Lose! " + getComputerChoice() + " beats " + getHumanChoice());
+            } else {
+                humanScore++;
+                return console.log("You Won!");
+            }
+        }
+
+        playRound();
+
+        roundsPlayed++;
+
+        finalMessage();
+
+        console.log("Computer Score: " + computerScore, "Human Score: " + humanScore);
+
+    }
+
+    while (roundsPlayed != rounds)
+}
+
+playGame();
